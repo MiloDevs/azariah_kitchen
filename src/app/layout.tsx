32b537e0b9d24457b7dadf276b98ac502d1dcafe
MiloDevs <siliconvalleyn91@gmail.com>
2023-11-07 17:41:12 +0300

    seo update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,10 +5,24 @@ import './globals.css'
 const josefin = Josefin_Sans({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: 'Azariah Kitchen',
-  description: 'A Food Blog',
+  title: "Azariah Kitchen: Food Blogging and Recipes",
+  description:
+    "Welcome to Kitchen Azariah, home of culinary delights, explore all varieties or recipes.",
   category: "Food",
-}
+  openGraph: {
+    type: "website",
+    url: "www.kitchenazariah.com",
+    title: "Azariah Kitchen: Food Blogging and Recipes",
+    description:
+      "Welcome to Kitchen Azariah, home of culinary delights, explore all varieties or recipes.",
+    siteName: "Azariah Kitchen",
+    images: [
+      {
+        url: "https://www.kitchenazariah.com/_next/image?url=%2Fazariahlogo.jpg&w=128&q=75",
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
